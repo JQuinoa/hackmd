6c97d44c139eb1065ac72c3a605dd8c9117fdb74
LluisArevalo <thorin119@gmail.com>
2017-01-04 13:52:42 +0100

    Create config.json file
    deploy script for heroku
    
    Heroku deploy
    
    Update deploy path
    
    Add heroku deployment bash file
    
    Remove public folder from gitignore
    
    Add the Ironhack Logo to the main page

---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    domain: '', // domain name
+    urlpath: '', // sub url path, like: www.example.com/<urlpath>
+
+    // settings
+    debug: false,
+
+    GOOGLE_API_KEY: '',
+    GOOGLE_CLIENT_ID: '',
+    DROPBOX_APP_KEY: ''
+};
